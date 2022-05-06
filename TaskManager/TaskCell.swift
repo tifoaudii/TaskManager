a9d4109ed2d8518e585c82960131822693dabd76
tifoaudii <tifoaudy@gmail.com>
2022-05-07 00:00:14 +0700

    add animation when scrolling

---
+++ TaskManager/TaskCell.swift
@@ -0,0 +1,17 @@
+//
+//  TaskCell.swift
+//  TaskManager
+//
+//  Created by Tifo Audi Alif Putra on 06/05/22.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+    
+    static let identifier = "TaskCellIdentifier"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- TaskManager/TaskViewController.swift
+++ TaskManager/TaskViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TaskViewController: UIViewController {
+class TaskViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var filterStackView: UIStackView!
     @IBOutlet weak var filterIndicatorLeadingConstraint: NSLayoutConstraint!
@@ -15,11 +15,49 @@ class TaskViewController: UIViewController {
     @IBOutlet weak var upcomingButton: UIButton!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var failedButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var titleTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var titleStackView: UIStackView!
+    
+    private var lastContentOffset: CGFloat = 0.0
+    private var maxYContentOffset: CGFloat = 80.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Task Manager"
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UINib(nibName: "TaskCell", bundle: .main), forCellReuseIdentifier: TaskCell.identifier)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: TaskCell.identifier, for: indexPath) as! TaskCell
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let contentOffset: CGFloat = scrollView.contentOffset.y
+        let percentage: CGFloat = contentOffset / 10
+        titleStackView.alpha = 1 - percentage
+        
+        if lastContentOffset > contentOffset {
+            if titleTopConstraint.constant < 0 && contentOffset < 50 {
+                titleTopConstraint.constant += 10
+                titleStackView.layoutIfNeeded()
+            }
+            
+        } else {
+            if titleTopConstraint.constant > -90 && contentOffset > 0 {
+                titleTopConstraint.constant -= 10
+                titleStackView.layoutIfNeeded()
+            }
+        }
+        
+        lastContentOffset = scrollView.contentOffset.y
     }
     
     @IBAction func onTapTodayButton(_ sender: Any) {
@@ -27,7 +65,6 @@ class TaskViewController: UIViewController {
         setInActive(for: upcomingButton, doneButton, failedButton)
         moveFilterIndicatorView(to: 0)
     }
-    
     
     @IBAction func onTapUpcomingButton(_ sender: Any) {
         upcomingButton.setTitleColor(.white, for: .normal)
@@ -53,10 +90,10 @@ class TaskViewController: UIViewController {
     }
     
     private func moveFilterIndicatorView(to index: Int) {
-        let leadingSpace = filterStackView.frame.width / 4 * CGFloat(index)
+        let leadingSpace: CGFloat = filterStackView.frame.width / 4 * CGFloat(index)
+        filterIndicatorLeadingConstraint.constant = leadingSpace
         
         UIView.animate(withDuration: 0.25) {
-            self.filterIndicatorLeadingConstraint.constant = leadingSpace
             self.view.layoutIfNeeded()
         }
     }
